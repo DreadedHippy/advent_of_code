562ddd0c1988585befceab5319009ea0693ede1e
onotaizee@gmail.com <onotaizee@gmail.com>
2023-12-03 17:58:12 +0100

    feat: day 2, puzzle 2

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -9,11 +9,12 @@ enum CubeVariant {
 #[derive(Debug)]
 struct CubeGroup(i32, CubeVariant);
 type CubeSet = Vec<CubeGroup>;
+type CubeCollection = Vec<CubeSet>;
 
 #[derive(Debug)]
 struct Game {
 	game_id: i32,
-	cube_set: CubeSet
+	cube_collection: CubeCollection
 }
 
 type PuzzleInput = Vec<Game>;
@@ -25,7 +26,7 @@ pub fn solve_puzzle_1(input_string: String) {
 	let mut sum_of_possible_ids = 0;
 
 	for input in puzzle_input {
-		let is_possible = input.cube_set.iter().map(|group| 
+		let is_possible = input.cube_collection.iter().flatten().map(|group| 
 			{
 				match &group {
 					CubeGroup(x, CubeVariant::Red) => x <= &12,
@@ -45,6 +46,33 @@ pub fn solve_puzzle_1(input_string: String) {
 
 }
 
+
+pub fn solve_puzzle_2(input_string: String) {
+	let puzzle_input = parse_puzzle(input_string);
+
+	let mut total_power_of_sets: usize = 0;
+
+	for input in puzzle_input {
+		let (mut min_red, mut min_green, mut min_blue) = (0, 0, 0);
+
+		for cube_set in input.cube_collection {
+			for group in cube_set {
+				match group {
+					CubeGroup(n, CubeVariant::Red) => {min_red = min_red.max(n)},
+					CubeGroup(n, CubeVariant::Blue) => {min_blue = min_blue.max(n)},
+					CubeGroup(n, CubeVariant::Green) => {min_green = min_green.max(n)},
+				}
+			}
+		}
+
+		total_power_of_sets+= (min_red * min_green * min_blue) as usize
+	}
+
+	println!("DAY 2, PUZZLE 2 ANSWER: {}", total_power_of_sets);
+
+
+}
+
 fn parse_puzzle(input_string: String) -> PuzzleInput {
 	let mut puzzle_input: PuzzleInput = Vec::new();
 
@@ -54,8 +82,7 @@ fn parse_puzzle(input_string: String) -> PuzzleInput {
 		// println!("{:?}", info);
 		let game_id = info[0].split_whitespace().last().map(|x| x.parse::<i32>().unwrap()).unwrap();
 
-		let mut cube_set: Vec<CubeGroup> = Vec::new();
-		let cube_collection: Vec<CubeSet> = Vec::new();
+		let mut cube_collection: Vec<CubeSet> = Vec::new();
 
 		let cubes = info[1].trim();
 
@@ -64,6 +91,7 @@ fn parse_puzzle(input_string: String) -> PuzzleInput {
 		for collection in collections {
 			let set = collection.split(",").map(|c| c.trim().to_string()).collect::<Vec<String>>();
 
+			let mut cube_set: Vec<CubeGroup> = Vec::new();
 			for group in set {
 				let cube_info = group.split_whitespace().map(|info| info.trim().to_string()).collect::<Vec<String>>();
 
@@ -80,11 +108,13 @@ fn parse_puzzle(input_string: String) -> PuzzleInput {
 
 				// println!("GROUP: {}",group);
 			}
+
+			cube_collection.push(cube_set)
 		}
 
 		// println!("{:?}",cube_set);
 
-		let game: Game =Game { game_id, cube_set };
+		let game: Game = Game { game_id, cube_collection };
 		puzzle_input.push(game);
 
 	};
--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,21 @@ use advent_of_code::{day1, day2};
 
 fn main() {    
 	// Day 1
-	// let mut input_file = File::open("src/day1/puzzle.txt").expect("Failed to find 'puzzle.txt'");
-	// let mut input_string = String::new();
-	// input_file.read_to_string(&mut input_string).expect("Failed to read file to string, invalid UTF-8 might be present");
+	let mut input_file = File::open("src/day1/puzzle.txt").expect("Failed to find 'puzzle.txt'");
+	let mut input_string = String::new();
+	input_file.read_to_string(&mut input_string).expect("Failed to read file to string, invalid UTF-8 might be present");
 
 
-    // day1::solve_puzzle_1(input_string.clone());
-    // day1::solve_puzzle_2(input_string);
+    day1::solve_puzzle_1(input_string.clone());
+    day1::solve_puzzle_2(input_string);
 
     // Day 2
 	let mut input_file = File::open("src/day2/puzzle.txt").expect("Failed to find 'puzzle.txt'");
 	let mut input_string = String::new();
 	input_file.read_to_string(&mut input_string).expect("Failed to read file to string, invalid UTF-8 might be present");
 
-    day2::solve_puzzle_1(input_string)
+    day2::solve_puzzle_1(input_string.clone());
+    day2::solve_puzzle_2(input_string)
 
 
 
