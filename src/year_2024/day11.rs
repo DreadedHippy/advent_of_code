134f021711edad3655e185a736aa4bf5fd43bb16
onotaizee@gmail.com <onotaizee@gmail.com>
2024-12-11 20:46:50 +0100

    feat: advent of code 2024 day 11

---
+++ src/year_2024/day11.rs
@@ -0,0 +1,58 @@
+use std::{collections::HashMap, fmt::format};
+
+pub fn day_11_first(input: String) {
+	let lines = input.lines().map(|x| x.split_whitespace().map(String::from).collect::<Vec<_>>()).collect::<Vec<_>>();
+
+	let mut l = lines[0].clone();
+	let mut memo = HashMap::new();
+
+	let mut sum: u128 = 0;
+
+	for n in l {
+		sum += transform(&"0".to_string(), &n, 0, 75, &mut memo) as u128;
+	}
+
+	println!("{:?}", sum);
+}
+
+fn transform(o: &String, curr: &String, od: usize, limit: usize, memo: &mut HashMap<(String, usize), usize>) -> usize {
+	// println!("{:?} {} {}",  curr, od, limit);
+	if od >= limit {
+		return 1;
+	}
+
+	if let Some(&v) = memo.get(&(curr.to_string(), limit-od)) {
+		return v
+	}
+
+	let mut ov = 0;
+	let mut nv = 0;
+
+	if curr == "0" {
+		let next = "1".to_string();
+		// ov += transform(o, &next,od + 1, limit, memo); // overall
+		nv += transform(&next, &next, od + 1, limit, memo); // overall
+	} else if curr.len() & 1 == 0 {
+		let l = curr[0..curr.len()/2].to_string();
+		let mut r = curr[(curr.len()/2)..].chars().skip_while(|&x| x == '0').collect::<String>();
+
+		if r.is_empty() {
+			r = "0".to_string()
+		}
+
+		for next in [l, r] {
+			// ov += transform(o,&next, od + 1, limit, memo); // overall
+			nv += transform(&next,&next, od + 1, limit, memo) // overall
+		}
+
+	} else {
+		let num = curr.parse::<u64>().unwrap();
+		let next = format!("{}", num * 2024);
+		// ov += transform(o, &next, od + 1, limit, memo); // overall
+		nv += transform(&next, &next, od + 1, limit, memo); // overall
+	}
+
+	// memo.insert((o.clone(), od + 1), ov);
+	memo.insert((curr.clone(), limit - od), nv);
+	return nv
+}
--- src/year_2024/mod.rs
+++ src/year_2024/mod.rs
@@ -4,6 +4,7 @@ use std::{collections::HashMap, time::Instant};
 // use day4::day_4_second;
 
 use day10::{day_10_first, day_10_second};
+use day11::day_11_first;
 use day5::{day_5_first, day_5_second};
 use day1::{day_1_first, day_1_second};
 use day2::{day_2_first, day_2_second};
@@ -26,6 +27,7 @@ pub mod day7;
 pub mod day8;
 pub mod day9;
 pub mod day10;
+pub mod day11;
 
 pub fn solve() {
 	// let input = read_puzzle_input("src/year_2024/day1.txt");
@@ -75,6 +77,9 @@ pub fn solve() {
 
     // let input = read_puzzle_input("src/year_2024/day10.txt");
     // day_10_first(input);
-    let input = read_puzzle_input("src/year_2024/day10.txt");
-    day_10_second(input);
+    // let input = read_puzzle_input("src/year_2024/day10.txt");
+    // day_10_second(input);
+
+    let input = read_puzzle_input("src/year_2024/day11.txt");
+    day_11_first(input);
 }
